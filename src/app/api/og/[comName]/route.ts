bc5747ad80fd7957c605e15d1cafa9dc4f9180e9
Allison <allisonloo@Allisons-MacBook-Pro.local>
2025-08-16 19:25:44 +0800

    minor tweak on nus

--- src/app/api/og/[comName]/route.ts
+++ src/app/api/og/[comName]/route.ts
@@ -132,6 +132,7 @@ export async function GET(
           {
             style: {
               display: "flex",
+              marginTop: "40px",
               flexDirection: "column",
               alignItems: "center",
               justifyContent: "center",
